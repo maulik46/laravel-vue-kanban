540690fab7e9e8dbd546e9dcbb7c2cbe160f2991
Maulik <maulikp4646@gmail.com>
2025-03-23 22:37:13 +0530

    Project title updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Kanban Board</title>
         @vite(['resources/css/app.css'])
         @inertiaHead
     </head>
